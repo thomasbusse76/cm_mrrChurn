d5c076f32fcd5d1cb5d24a82cce7f11a30951175
Thomas Busse <gh@interzone-media.com>
2017-07-23 21:16:03 +0200

    Cleaned up code
    Cleaned up indentations etc

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 require('./vendor/autoload.php');
 
 ChartMogul\Configuration::getDefaultConfiguration()
-->setAccountToken('2bb03f684bbc42051654721dfbf34375')
-->setSecretKey('3dbbe4120d1328eee0f3fe4a129b96cb');
+->setAccountToken('YOUR_ACCOUNT_TOKEN')
+->setSecretKey('YOUR_ACCOUNT_KEY');
 
 $mrrChurn =  (ChartMogul\Metrics::mrrChurnRate([
     'start-date'  => '2015-08-01',
@@ -14,22 +14,20 @@ $mrrChurn =  (ChartMogul\Metrics::mrrChurnRate([
 $mrrChurnEntries = $mrrChurn->toArray()['entries'];
 
 //Sorting the array by churn rate in descending order
-usort($mrrChurnEntries, function($a, $b) {
-	if ($a['mrr_churn_rate'] == $b['mrr_churn_rate']) {
-		return 0;
-	}
+usort($mrrChurnEntries, function ($a, $b) {
+    if ($a['mrr_churn_rate'] == $b['mrr_churn_rate']) {
+        return 0;
+    }
 
-	return ($a['mrr_churn_rate'] > $b['mrr_churn_rate']) ? -1 : 1;
+    return ($a['mrr_churn_rate'] > $b['mrr_churn_rate']) ? -1 : 1;
 });
 
 // print the 3 months with the highest Churn rates
 echo '<br /><br />The three months with the highest churn rates are:<br /><br />';
 
 for ($x = 0; $x < 3; $x++) {
-	$time = strtotime($mrrChurnEntries[$x]["date"]);
-	$month = date("F", $time);
-	$year = date("Y", $time);
-  print_r("-" . $month . " " . $year . " (" . $mrrChurnEntries[$x]["mrr_churn_rate"] . "%) <br />");
+    $time = strtotime($mrrChurnEntries[$x]["date"]);
+    $month = date("F", $time);
+    $year = date("Y", $time);
+    print_r("-" . $month . " " . $year . " (" . $mrrChurnEntries[$x]["mrr_churn_rate"] . "%) <br />");
 }
-
-?>
